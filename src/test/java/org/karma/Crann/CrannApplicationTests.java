6102c592f8c6931c116ba3885f713f4b8e15c80c
Alex K'rma <vestagamesoffcial@gmail.com>
2024-09-03 20:07:38 +0700

    First commit

---
+++ src/test/java/org/karma/Crann/CrannApplicationTests.java
@@ -0,0 +1,13 @@
+package org.karma.Crann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrannApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
