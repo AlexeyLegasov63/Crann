3a046ae162e519992c77b0407b45ab55da88f940
Alex K'rma <vestagamesoffcial@gmail.com>
2024-09-14 20:18:40 +0700

    First commit

---
+++ src/main/java/org/karma/crann/model/Board.java
@@ -0,0 +1,10 @@
+package org.karma.crann.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "boards")
+public class Board {
+
+}
--- src/main/java/org/karma/crann/model/User.java
+++ src/main/java/org/karma/crann/model/User.java
@@ -1,9 +1,15 @@
 package org.karma.crann.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.io.Serializable;
 
+@Data
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements Serializable {
+	public static final long serialVersionID = 23492940340389424L;
+
 }
