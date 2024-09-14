3d4e6e2e0891191c528e36f2b7545fb5aedc5564
Alex K'rma <vestagamesoffcial@gmail.com>
2024-09-14 19:36:43 +0700

    First commit

--- src/main/java/org/karma/crann/model/Note.java
+++ src/main/java/org/karma/crann/model/Note.java
@@ -1,10 +1,20 @@
 package org.karma.crann.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import lombok.Data;
 
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
 @Entity
 @Table(name = "notes")
-public class Note {
-
+public class Note implements Serializable {
+	@Serial
+	private static final long serialVersionUID = 2245613874502221677L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String email, password;
+	private boolean enabled;
 }
